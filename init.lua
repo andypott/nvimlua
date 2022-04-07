96df3f285a227327796a69dafb85ca0a0ea0014a
Andy Potter <andy@andypotter.co.uK>
2022-04-07 21:47:22 +0100

    Add treesitter

--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
-require('packer-plugins')
+require('plugins.packer')
+require('plugins.lsp-config')
+require('nvim-treesitter.configs').setup {
+    highlight = { enable = true },
+}
 require('options')
 require('keybindings')
-require('lsp-config')
 
 -- add patched font to enable icons
 -- https://www.nerdfonts.com/font-downloads
